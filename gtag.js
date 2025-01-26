08f39f7f2b5a2fdc68296888fbb3f58319751351
DA-TENSHI <ttaiga1324@gmail.com>
2025-01-26 06:19:13 +0000

    up

---
+++ gtag.js
@@ -0,0 +1,7 @@
+document.write('<script async src="https://www.googletagmanager.com/gtag/js?id=G-7WKDL1V0ZS"></script>');
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-7WKDL1V0ZS');
---
+++ javascript.js
@@ -0,0 +1,9 @@
+console.log("こんにちは");
+console.log("信濃眞伊のウェブサイトへようこそ");
+console.log("特に面白味もありませんが、ごゆっくりどうぞ");
+console.log("ちなみに、");
+console.log("信濃眞伊はJavascriptがかけません");
+console.log("Javascript.js更新日");
+console.log("皇紀2684年9月2日月曜日");
+console.log("Whitten by SHINANOMAI");
+console.log("CopyRight© 2022-2024(皇紀2682-2684) SHINANOMAI All rights reserved.");
